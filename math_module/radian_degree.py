03fc9cf1db1554b8a523e1f28fa972ad928abaaf
Paras Upadhyay <114799800+paras-13@users.noreply.github.com>
2022-12-01 20:30:49 +0530

    Radian and Degree conversions

---
+++ math_module/radian_degree.py
@@ -0,0 +1,9 @@
+# Degree and radian conversions
+# Code -->
+from math import radians,degrees
+
+# Degree to radian
+print("In radiams --> ",radians(int(input("Enter a degree"))))
+
+# Radian to degree
+print("In degrees --> ",degrees(float(input("Enter value in radians"))))
